9cf1bea75a334663216115294c16878695a8f543
Suns Jay <sunsjay0806@gmail.com>
2024-05-15 18:10:18 +0800

    refactor: change zh-cn

--- app/Main.tsx
+++ app/Main.tsx
@@ -59,7 +59,7 @@ export default function Home({ posts }) {
                           className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                           aria-label={`Read more: "${title}"`}
                         >
-                          Read more &rarr;
+                          阅读更多 &rarr;
                         </Link>
                       </div>
                     </div>
@@ -77,15 +77,15 @@ export default function Home({ posts }) {
             className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label="All posts"
           >
-            All Posts &rarr;
+            所有文章 &rarr;
           </Link>
         </div>
       )}
-      {siteMetadata.newsletter?.provider && (
+      {/* {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
-      )}
+      )} */}
     </>
   )
 }
